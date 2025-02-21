f42696b2fafa4e79c10c45f91ea276dc0e539098
devcarloshenrique <ocarlosh2024@gmail.com>
2025-02-21 12:39:44 -0300

    feat: create use case create-gym with create method and sucess test

---
+++ src/use-cases/create-gym.spec.ts
@@ -0,0 +1,25 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { CreateGymUseCase } from './create-gym'
+
+let gymsRepository: InMemoryGymsRepository
+let sut: CreateGymUseCase
+
+describe('Create Gym Use Case', () => {
+  beforeEach(() => {
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new CreateGymUseCase(gymsRepository)
+  })
+
+  it('should be able to create gym', async () => {
+    const { gym } = await sut.execute({
+      title: 'Fabrica de monstros',
+      phone: null,
+      description: null,
+      latitude: -3.795195,
+      longitude: -38.5487757,
+    })
+
+    expect(gym.id).toEqual(expect.any(String))
+  })
+})
